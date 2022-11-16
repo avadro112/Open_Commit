39a4529a1118dae39f4b24c95180a705f39be54c
avadro112 <jkanant@gmail.com>
2022-11-17 00:05:21 +0530

    adding typo

---
+++ typeo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//insert swap function without using third variable 
+int swap(const int &a,const int &b){
+    //code
+}
+
+int main ()
+{
+    int num = 10;
+    int num1 = 1000;
+    swap(num,num1);
+    cout<<
+
+}
